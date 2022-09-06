7fb379540d2e6e9577cae0343e7fbfd67ce7861d
Nikhil <nikhil.shah2008@gmail.com>
2022-09-06 10:20:25 +0530

    WEBAIPI EF

---
+++ day 3/shoppingWebAPI_EF/Models/EF/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace shoppingWebAPI_EF.Models.EF
+{
+    public partial class Customer
+    {
+        public int CId { get; set; }
+        public string? CName { get; set; }
+        public string? CCategory { get; set; }
+        public string? CLocation { get; set; }
+        public int? CWallet { get; set; }
+        public bool? CIsActive { get; set; }
+    }
+}
